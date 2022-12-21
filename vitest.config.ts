113dd3dd6f6fed1e417fa653f98479aa5fb4a98d
lynn <jcignorance@outlook.com>
2022-12-21 14:22:55 +0800

    fix: lcov.info not found

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,10 @@ export default defineConfig({
     ]
   },
   test: {
-    globals: true
+    globals: true,
+    coverage: {
+      reporter: ['lcov'],
+      reportsDirectory: './coverage'
+    }
   }
 })
