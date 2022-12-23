08d8a8047f60bb9f9c3153ad6253aa649109d7f6
lynn <jcignorance@outlook.com>
2022-12-23 15:28:37 +0800

    feat: add `retry` function (#5)
    * feat: add
    
    * chore: delete useless code

--- test/function.spec.ts
+++ test/function.spec.ts
@@ -1,21 +1,42 @@
-import { sleep, noop } from "@/function"
+import { sleep, noop, retry } from "@/function"
 
 describe('function', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
   it('should work with sleep', async () => {
-    let a = 0
-    setTimeout(() => {
-      expect(a).toBe(0)
-    }, 1000)
-  
-    setTimeout(() => {
-      expect(a).toBe(1)
-    }, 4000)
-  
-    await sleep(3)
-    ++a
+    const fn = vi.fn()
+    const p = sleep(3000)
+    vi.runAllTimers()
+    await p
+    fn()
+    expect(fn).toBeCalled()
   })
 
   it('noop should work', () => {
     expect(noop()).toBeUndefined()
+  })
+
+  it('should be worked with retry', async () => {
+    const resolve = vi.fn(() => Promise.resolve('resolve'))
+    const reject = vi.fn(() => Promise.reject('reject'))
+
+    try {
+      await retry(reject)
+    } catch (e) {
+      expect(e).toBe('reject')
+      expect(reject).toHaveBeenCalledTimes(3)
+    }
+
+    try {
+      const res = await retry(resolve)
+      expect(res).toBe('resolve')
+      expect(resolve).toHaveBeenCalledTimes(1)
+    } catch (e) {
+      //
+    }
   })
 })
