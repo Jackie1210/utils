db2ba0b0078ec6361ac7f7f6f2c9b683499b7aba
lynn <jcignorance@outlook.com>
2022-12-23 15:23:51 +0800

    feat: add

--- src/function.ts
+++ src/function.ts
@@ -9,3 +9,14 @@ export function sleep(ms: number = 3) {
     }, ms * 1000)
   })
 }
+
+export async function retry<T>(fn: (...v: any[]) => Promise<T> | T, times = 3): Promise<T> {
+  try {
+    return await fn()
+  } catch (e) {
+    if (times === 1) {
+      throw e
+    }
+    return retry(fn, --times)
+  }
+}
--- test/function.spec.ts
+++ test/function.spec.ts
@@ -1,21 +1,47 @@
-import { sleep, noop } from "@/function"
+import { sleep, noop, retry } from "@/function"
+
+const executeAfterTwoHours = (func: any) => {
+  setTimeout(func, 1000 * 60 * 60 * 2) // 2 hours
+}
+
 
 describe('function', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
   it('should work with sleep', async () => {
-    let a = 0
-    setTimeout(() => {
-      expect(a).toBe(0)
-    }, 1000)
-  
-    setTimeout(() => {
-      expect(a).toBe(1)
-    }, 4000)
-  
-    await sleep(3)
-    ++a
+    const fn = vi.fn()
+    const p = sleep(3000)
+    vi.runAllTimers()
+    await p
+    fn()
+    expect(fn).toBeCalled()
   })
 
   it('noop should work', () => {
     expect(noop()).toBeUndefined()
+  })
+
+  it('should be worked with retry', async () => {
+    const resolve = vi.fn(() => Promise.resolve('resolve'))
+    const reject = vi.fn(() => Promise.reject('reject'))
+
+    try {
+      await retry(reject)
+    } catch (e) {
+      expect(e).toBe('reject')
+      expect(reject).toHaveBeenCalledTimes(3)
+    }
+
+    try {
+      const res = await retry(resolve)
+      expect(res).toBe('resolve')
+      expect(resolve).toHaveBeenCalledTimes(1)
+    } catch (e) {
+      //
+    }
   })
 })
